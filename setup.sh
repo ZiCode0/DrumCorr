2e1f85dd57ecc8a417f2b6a10037ae041857550f
ZiCode0 <zedcode.05@gmail.com>
2020-09-29 14:40:32 +1200

    :wrench: download project update script :wrench:

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+wget https://gist.githubusercontent.com/mbenford/79d8771653dfbc1ce92a/raw/5370456da0274551ccf01b2a053d0c8000d40558/git-update.sh
 python -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
