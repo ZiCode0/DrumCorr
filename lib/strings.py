3c556ff426f9d91318202c7a0dd6e8901a5ba614
ZiCode0 <zed.code.05@gmail.com>
2020-12-18 16:16:34 +1200

    :loud_sound: update log strings :loud_sound:

--- lib/strings.py
+++ lib/strings.py
@@ -2,7 +2,10 @@ __project_name__ = 'DrumCorr'
 
 
 class Console:
+    start_init = 'Start program init..'
     program_start = 'Program {project_name} started.'.format(project_name=__project_name__)
+    reading_template = 'Reading template from <{template}>..'
+    process_loaded_files = 'Processing {count} files in progress'
     calc_file_finished = 'File <{input_file}> report calculated, elapsed time: {elapsed_time}'
 
 
