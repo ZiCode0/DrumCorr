a32a31c20275b7f80a07afc28bab4468934c2ba8
ZiCode0 <zed.code.05@gmail.com>
2020-12-21 19:10:24 +1200

    :loud_sound: some update :loud_sound:

--- lib/strings.py
+++ lib/strings.py
@@ -5,7 +5,7 @@ class Console:
     start_init = 'Start program init..'
     program_start = 'Program {project_name} started.'.format(project_name=__project_name__)
     reading_template = 'Reading template from <{template}>..'
-    process_loaded_files = 'Processing {count} files in progress'
+    process_loaded_files = 'Processing {count} files in progress..'
     calc_file_finished = 'File <{input_file}> report calculated, elapsed time: {elapsed_time}'
     low_corr_warning = 'Cross-correlation returns 0 or low ({res_num}) number of results of' \
                        '\n    file: <{file_name}>, quit..'
