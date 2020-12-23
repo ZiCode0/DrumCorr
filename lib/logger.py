c3b830241a3d56b4d8356260a66b8b0a8282c27f
ZiCode0 <zed.code.05@gmail.com>
2020-12-23 21:38:43 +1200

    :recycle: init logger changes :recycle:

--- lib/logger.py
+++ lib/logger.py
@@ -3,14 +3,16 @@ from loguru import logger
 from lib import notify
 
 
-def init_logger(program_name, log_level='DEBUG'):
+def init_logger(project_name, notify_providers=None, log_level='DEBUG'):
     """
-    Logger for program
-    :type program_name: name of current program
+    Logger for program.
+    Use providers to add report errors.
+    :type project_name: name of current program
+    :param notify_providers: list of notification providers if needed
     :type log_level: log level to filter output
     """
     # set log format
-    logger.add('{program_name}.log'.format(program_name=program_name),
+    logger.add('{project_name}.log'.format(project_name=project_name),
                format='{time:!UTC} {level} {message}',
                level=log_level,
                rotation='1 MB',
