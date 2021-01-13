3695d04aa9c2a9429f845fd17ac9d67ab9ff0f65
ZiCode0 <zed.code.05@gmail.com>
2021-01-13 14:29:11 +1200

    :wrench: modify update script :wrench:

--- update.sh
+++ update.sh
@@ -5,31 +5,46 @@ local_branch=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 remote_branch=$(git rev-parse --abbrev-ref --symbolic-full-name @{u})
 remote=$(git config branch."$local_branch".remote)
 
+git_updated=false
+
 echo "Fetching from $remote..."
 git fetch "$remote"
 
 # shellcheck disable=SC2086
 if git merge-base --is-ancestor $remote_branch HEAD; then
     echo 'Already up-to-date'
-    exit 0
+    # shellcheck disable=SC2034
+    git_updated=true
+    # exit 0
 fi
 
-if git merge-base --is-ancestor HEAD "$remote_branch"; then
-    echo 'Fast-forward possible. Merging...'
-    # shellcheck disable=SC2086
-    git merge --ff-only --stat $remote_branch
-else
-    echo 'Fast-forward not possible. Rebasing...'
-    git rebase --preserve-merges --stat "$remote_branch"
+# shellcheck disable=SC2078
+if [ ! git_updated ]; then
+    if git merge-base --is-ancestor HEAD "$remote_branch"; then
+        echo 'Fast-forward possible. Merging...'
+        # shellcheck disable=SC2086
+        git merge --ff-only --stat $remote_branch
+    else
+        echo 'Fast-forward not possible. Rebasing...'
+        git rebase --preserve-merges --stat "$remote_branch"
+    fi
 fi
 
-DIR_SCRIPT=$(pwd) # Current directory
+# upgrade Python virtual environment
+DIR_SCRIPT=$(pwd) # set current directory
 
-# create new venv if not exist
+# create venv if not exist
 if [ ! -d "$DIR_SCRIPT"/venv ]; then
   python -m venv venv;
 fi
 
+# pip upgrade
 source "$DIR_SCRIPT"/venv/bin/activate
-pip install -r requirements.txt
+pip install -r requirements.txt --disable-pip-version-check
 deactivate
+
+# clear
+CLR="\033[1;34m"
+NCLR="\033[0m"
+# shellcheck disable=SC2027
+echo -e """${CLR}""Update Success..""${NCLR}"
