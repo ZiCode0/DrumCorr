7c1d0632d308d5ba578f7daa37f5d6d0c4cbdc5e
ZiCode0 <zed.code.05@gmail.com>
2021-03-03 12:27:52 +1200

    :sparkles: add calibration instance :sparkles:

---
+++ lib/extra/calibration.py
@@ -0,0 +1,10 @@
+class Calibrations:
+    def __init__(self, chars_dict):
+        self.src_chars_dict = chars_dict
+        self.v = {'amplitude_multiplier': self.get_amplitude_multiplier()
+                  }
+        del self.src_chars_dict
+
+    def get_amplitude_multiplier(self):
+        return float(self.src_chars_dict['CHINFONEED'][7])
+        # print(chars_dict)
