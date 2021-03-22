755e0fa6a50a1ba1c2dd291a7f4f5dc6e3600008
ZiCode0 <zed.code.05@gmail.com>
2021-03-23 10:47:31 +1200

    :construction: try multiple graph plot :construction:

--- lib/extra/plot.py
+++ lib/extra/plot.py
@@ -2,14 +2,19 @@ import plotly.express as px
 import datetime
 
 
-def fancy_plot(input_trace):
+def fancy_plot(input_traces):
     """
     Draw fancy interactive time series using plotly library
-    :param input_trace: target trace to draw
+    :param input_traces: target trace to draw
     """
-    y = input_trace.data
+    if type(input_traces) is list:
+        y = [i.data for i in input_traces]  # input_trace.data
+    else:
+        y = input_traces.data
+
     x = [datetime.datetime.fromtimestamp(
-        input_trace.stats.starttime.datetime.timestamp() + i)
-        for i in input_trace.times('relative').tolist()]
-    fig = px.scatter(x=x, y=y)
+        input_traces[0].stats.starttime.datetime.timestamp() + i)
+        for i in input_traces[0].times('relative').tolist()]
+
+    fig = px.line(x=x, y=y)
     fig.write_html('first_figure.html', auto_open=True)
