cb7a6f826f63d9f0ef43a74c700788b26f60b967
ZiCode0 <zed.code.05@gmail.com>
2021-03-23 16:01:49 +1200

    :sparkles: return average data :sparkles:

--- lib/extra/amplitude.py
+++ lib/extra/amplitude.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 def return_micron_to_seconds(input_amp):
     """
     Return value in micron/seconds format
@@ -6,3 +9,23 @@ def return_micron_to_seconds(input_amp):
     """
     visual_multiplier = 1000000  # 1 000 000
     return round(input_amp * visual_multiplier, 4)
+
+
+def average_amplitude(detects):
+    """
+    Calculate average amplitude value
+    :param detects:
+    :return:
+    """
+    return sum([i['max_amplitude'] for i in detects]) / len(detects)
+
+
+def average_delta_time(detects):
+    times = [i['max_amplitude_time'] for i in detects]
+    deltas = []
+    # calculate deltas list
+    for time_index in range(len(times)-1):
+        deltas.append(times[time_index+1].datetime - times[time_index].datetime)
+    # calculate average deltas
+    average_delta = sum(deltas, datetime.timedelta(0)) / len(deltas)
+    return average_delta
