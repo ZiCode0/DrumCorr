2f082a1f6a24122b7040185ac03d52bb976c21ed
ZiCode0 <zed.code.05@gmail.com>
2022-02-17 16:48:16 +1200

    :art: improve comment format :art:

--- main.py
+++ main.py
@@ -17,45 +17,53 @@ def main():
     """
     Main program function
     """
-    ca = ConsoleApp()  # console app instance
-    sr = StreamReader()  # file reader instance
-    conf = JsonConfig(ca.args.config)  # config instance
+    # console app instance
+    ca = ConsoleApp()
+    # file reader instance
+    sr = StreamReader()
+    # config instance
+    conf = JsonConfig(ca.args.config)
+    # init logger
     logger_lib.init_logger(project_name=strings.__project_name__,
-                           notify_providers=conf.param['notify'])  # init logger
-    logger.info(strings.Console.start_init)  # log: init program
-    dc = DrumCorr()  # DrumCorr instance
-    logger.info(strings.Console.program_start)  # log: start program
-
-    _template_path, _file_paths = file_parser(conf)  # get files list
+                           notify_providers=conf.param['notify'])
+    # log: init program
+    logger.info(strings.Console.start_init)
+    # DrumCorr instance
+    dc = DrumCorr()
+    # log: start program
+    logger.info(strings.Console.program_start)
+    # get files list
+    _template_path, _file_paths = file_parser(conf)
+    # log: read template
     logger.info(strings.Console.reading_template.format(
-        template=os.path.basename(_template_path)))  # log: read template
-    t_template_object, t_template_chars = sr.read(
-        path=_template_path)  # dc.get_template(template_path)  # read template
-
+        template=os.path.basename(_template_path)))
+    # read template
+    t_template_object, t_template_chars = sr.read(path=_template_path)
+    # apply instrumental correction to template file
     dc.remove_response(stream=t_template_object,
-                       fdsn_url=conf.param['fdsn_server_url'])  # apply instrumental correction to template file
-
+                       fdsn_url=conf.param['fdsn_server_url'])
+    # filter template
     dc.filter_data(t_template_object,  # data
                    conf.param['filter']['filter_name'],  # filter name
                    **conf.param['filter']['filter_params'])  # filter parameters
+    # log: info about loaded files
     logger.info(strings.Console.process_loaded_files.format(
-        count=len(_file_paths)))  # log: info about loaded files
-    for file_index in range(len(_file_paths)):  # processing files
-        _timer_start = time.process_time()  # start file processing timer
+        count=len(_file_paths)))
+    # processing files
+    for file_index in range(len(_file_paths)):
+        # start file processing timer
+        _timer_start = time.process_time()
         # get file
         _file_path = _file_paths[file_index]
         # file name to results
         dc.workspace.current_file_name = os.path.basename(_file_paths[file_index])
         # detection value for xcorr
         dc.workspace.detection_value = conf.param['xcorr_detection_value']
-
         # get file content
         dc.workspace.stream = dc.read_file(_file_path)
-
         # apply instrumental correction to input file
         dc.remove_response(stream=dc.workspace.stream,
                            fdsn_url=conf.param['fdsn_server_url'])
-
         # filter input file data
         dc.filter_data(dc.workspace.stream,
                        conf.param['filter']['filter_name'],
@@ -67,7 +75,8 @@ def main():
         # skip file if correlation results is low
         if not dc.check_xcorr_results(template_minimum_count=conf.param['xcorr_minimum_count']):
             continue
-        dc.workspace.approx_xcorr = dc.approx_xcorr(detections=dc.workspace.detects)  # calculate approximate corr
+        # calculate approximate corr
+        dc.workspace.approx_xcorr = dc.approx_xcorr(detections=dc.workspace.detects)
         # calculate correlation maximum of file
         dc.get_max_amplitudes()
         # generate report name
@@ -79,7 +88,8 @@ def main():
         logger.info(strings.Console.calc_file_finished.format(
             input_file=dc.workspace.current_file_name,
             elapsed_time=time.process_time() - _timer_start))
-        dc.clean_report()  # clean report object
+        # clean report object
+        dc.clean_report()
     # log: exit program
     logger.info(strings.Console.program_exit.format(project_name=strings.__project_name__,
                                                     project_folder=conf.param['data_folder'],
